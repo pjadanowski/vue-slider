165e3159cc423374c28feed6c353a7c647a892db
Paweł Jadanowski <pjadanowski@gmail.com>
2019-08-22 23:10:19 +0200

    startSlide

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,24 @@ export default {
               'https://picsum.photos/id/1000/400',
               'https://picsum.photos/id/1001/400',
               'https://picsum.photos/id/1002/400',
-      ]
+      ],
+      currentSlide: 0,
+      timer: null
+    }
+  },
+  methods: {
+    startSlide() {
+      this.timer = setInterval(this.next, 3000)
+    },
+    next() {
+      this.currentSlide += 1
     }
   },
   components: {
     Gallery
+  },
+  mounted() {
+    this.startSlide()
   }
 }
 </script>
